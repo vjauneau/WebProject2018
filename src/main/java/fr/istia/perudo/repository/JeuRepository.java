f003d898fcf2acf890eb1967bf4f9a8904566f8b
usrlocal <usrlocal@GA319-FE06.ad.univ-angers.fr>
2018-10-24 12:17:22 +0200

    generate des jeu

--- src/main/java/fr/istia/perudo/repository/JeuRepository.java
+++ src/main/java/fr/istia/perudo/repository/JeuRepository.java
@@ -1,7 +1,12 @@
 package fr.istia.perudo.repository;
 
 import fr.istia.perudo.domain.Jeu;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -12,4 +17,15 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface JeuRepository extends JpaRepository<Jeu, Long> {
 
+	
+	@Query("select count(j) from Jeu j where j.nbDe = :nbDe")
+	int countWhere(@Param("nbDe")int nbDe);
+
+	
+	@Query("select j from Jeu j where j.nbDe = :nbDe and id = :id")
+	Jeu findOneRandom(@Param("nbDe")int nbDe,@Param("id") int id);
+
+	
+	@Query("select j.id from Jeu j where j.nbDe = :nbDe")
+	List<Long> getidforfirstinlist(Pageable pageable, int nbDe);
 }
--- src/main/java/fr/istia/perudo/service/JeuService.java
+++ src/main/java/fr/istia/perudo/service/JeuService.java
@@ -4,35 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import org.springframework.data.domain.PageRequest;
+
 import fr.istia.perudo.domain.De;
+import fr.istia.perudo.domain.Jeu;
 import fr.istia.perudo.repository.DeRepository;
+import fr.istia.perudo.repository.JeuRepository;
 
 public class JeuService {
 	
+	private static JeuRepository jeuRepository;
 	
-	private DeRepository deRepository;
 	
-	public JeuService()
+	public JeuService(JeuRepository JeuRepository)
 	{
-		
+		this.jeuRepository = JeuRepository;
 	}
 	
 	public List<De> GenerateJeu()
 	{
 		
-		List<De> allDe = this.deRepository.findAll();
-		List<De> resultSet = new ArrayList<>();
- 		int count = allDe.size();
+
+		return null;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public Jeu GetNewJeu(int nbDe)
+	{
+		int count = this.jeuRepository.countWhere(nbDe);
+		Random random = new Random();
+		int id = random.nextInt(count);
+		List<Long> idfirst = this.jeuRepository.getidforfirstinlist(new PageRequest(0,1), nbDe);
+		id += idfirst.get(0);
+		Jeu result = this.jeuRepository.findOneRandom(nbDe,id);
 		
-		for(int i=0; i< 6 ;i++)
-		{
-		Random rand = new Random();
-		int number = rand.nextInt(count);
-		resultSet.add(allDe.get(rand.nextInt(count)));
-		}
+		return result;
 		
-		
-		return resultSet;
 	}
 
 }
